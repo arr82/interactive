7ae4efd8d93c3507f846f25f72d84433002a99e8
arr82 <ainsley.romero@yale.edu>
2022-03-08 13:20:47 -0500

    key

---
+++ keyboard_instrument/main.js
@@ -0,0 +1,89 @@
+// this is where you write your javascript
+
+$(window).keydown(function(e){
+
+  var key = e.key;
+
+  console.log(key);
+
+  if (key == "a") {
+    $(".A").toggle();
+  }
+  else if (key == "b") {
+    $(".B").toggle("");
+  }
+  else if (key == "c") {
+    $(".C").toggle("");
+  }
+  else if (key == "d") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "e") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "f") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "g") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "h") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "i") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "j") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "k") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "l") {
+    $(".heart").toggle("");
+  }
+  else if (key == "m") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "n") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "o") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "p") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "q") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "r") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "s") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "t") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "u") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "v") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "w") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "x") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "y") {
+    $(".message").html("Cadillac");
+  }
+  else if (key == "z") {
+    $(".message").html("Cadillac");
+  }
+
+
+});
