1e85b459a4df357f3c37a6574441d6a04e2fef25
arr82 <ainsley.romero@yale.edu>
2022-04-12 11:07:35 -0400

    changes

--- loop/finalloop.js
+++ loop/finalloop.js
@@ -6,34 +6,3 @@ function saveID(id) {
     $.cookie('idCookie', id);
   }
 }
-
-// make all links colored
-function setVisted() {
-  var idArray = $.cookie('idCookie').split(',');
-  for (var x=0; x<idArray.length; x++) {
-    $('#' + idArray[x]).css('color', '#ff0000');
-  }
-}
-
-// assign saveID()
-$(document).ready(function(){
-  $('a').click(function(){
-    saveId($(this).attr('id'));
-  });
-  setVisited();
-});
-
-
-$("#section60").click(
-  function () {
-     var newDiv = $("<div>").append(finalloop.HTML).click(function () { alert("click!"); });
-     $(this).append(newDiv);
-  }
-)
-
-$("#section1").click(
-  function () {
-     var newDiv = $("<div>").append(finalloop.HTML).click(function () { alert("click!"); });
-     $(this).preppend(newDiv);
-  }
-)
